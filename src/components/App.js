1514f75f3e244a290cf01e95371d6b7e0c100093
canasmh <canasmh@yahoo.com>
2022-11-21 19:44:47 -0500

    Use newly created actions to create the action for the dispatch

--- src/components/App.js
+++ src/components/App.js
@@ -35,27 +35,27 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} onClick={() => dispatch({type: APPLY_NUMBER, payload: 1})}/>
-              <CalcButton value={2} onClick={() => dispatch({type: APPLY_NUMBER, payload: 2})}/>
-              <CalcButton value={3} onClick={() => dispatch({type: APPLY_NUMBER, payload: 3})}/>
+              <CalcButton value={1} onClick={() => dispatch(applyNumber(1))}/>
+              <CalcButton value={2} onClick={() => dispatch(applyNumber(2))}/>
+              <CalcButton value={3} onClick={() => dispatch(applyNumber(3))}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4} onClick={() => dispatch({type: APPLY_NUMBER, payload: 4})}/>
-              <CalcButton value={5} onClick={() => dispatch({type: APPLY_NUMBER, payload: 5})}/>
-              <CalcButton value={6} onClick={() => dispatch({type: APPLY_NUMBER, payload: 6})}/>
+              <CalcButton value={4} onClick={() => dispatch(applyNumber(4))}/>
+              <CalcButton value={5} onClick={() => dispatch(applyNumber(5))}/>
+              <CalcButton value={6} onClick={() => dispatch(applyNumber(6))}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7} onClick={() => dispatch({type: APPLY_NUMBER, payload: 7})}/>
-              <CalcButton value={8} onClick={() => dispatch({type: APPLY_NUMBER, payload: 8})}/>
-              <CalcButton value={9} onClick={() => dispatch({type: APPLY_NUMBER, payload: 9})}/>
+              <CalcButton value={7} onClick={() => dispatch(applyNumber(7))}/>
+              <CalcButton value={8} onClick={() => dispatch(applyNumber(8))}/>
+              <CalcButton value={9} onClick={() => dispatch(applyNumber(9))}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"} onClick={() => dispatch({type: CHANGE_OPERATION, payload: "+"})}/>
-              <CalcButton value={"*"} onClick={() => dispatch({type: CHANGE_OPERATION, payload: "*"})}/>
-              <CalcButton value={"-"} onClick={() => dispatch({type: CHANGE_OPERATION, payload: "-"})}/>
+              <CalcButton value={"+"} onClick={() => dispatch(changeOperation("+"))}/>
+              <CalcButton value={"*"} onClick={() => dispatch(changeOperation("*"))}/>
+              <CalcButton value={"-"} onClick={() => dispatch(changeOperation("-"))}/>
             </div>
 
             <div className="row ce_button">
