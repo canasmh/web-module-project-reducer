455b07ce68cf662c59823018fed6442c713f564a
canasmh <canasmh@yahoo.com>
2022-11-21 19:32:43 -0500

    Create a clear display action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 export const ADD_ONE = "ADD_ONE";
-
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 
