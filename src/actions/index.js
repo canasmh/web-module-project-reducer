9455e5ecabe96ea20f53e5826d0a239812090544
canasmh <canasmh@yahoo.com>
2022-11-21 19:43:24 -0500

    Create action functions that returns the reducer type and payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ export const ADD_ONE = "ADD_ONE";
 export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const ADD_MEMORY = "ADD_MEMORY";
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +10,12 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operation) => {
+    return ({type: CHANGE_OPERATION, payload: operation})
+}
+
+export const addMemory = (total) => {
+    return({type:ADD_MEMORY, payload:total})
 }
